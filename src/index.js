bf5eda28ef911f6c087f00add0f07d09ee80f622
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-12 09:48:36 +0200

    create index.js file

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import store from './redux/configStore';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+);
